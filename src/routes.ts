92ee5f5454ca5e1d2c3be83899e461e66b368cfb
glenn <70140106+ausgore@users.noreply.github.com>
2024-12-10 17:33:06 +0800

    API /reply

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,20 @@
-import { FastifyPluginAsyncTypebox } from "@fastify/type-provider-typebox";
+import { FastifyPluginAsyncTypebox, Type } from "@fastify/type-provider-typebox";
 import maslow from "./maslow";
+import { Message, NEEDS } from "./types";
 
 const routes: FastifyPluginAsyncTypebox = async (app) => {
 	app.get('/start', async (req, res) => {
 		return maslow.start();
 	});
 
-	app.get('/reply', async (req, res) => {
-
+	app.post<{
+		Body: {
+			content: string;
+			history: Message[];
+			need: NEEDS;
+		}
+	}>('/reply', async (req, res) => {
+		return maslow.reply(req.body.content, req.body.history, req.body.need);
 	});
 }
 
