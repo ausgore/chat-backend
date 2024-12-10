b74a93aac1a528d639a0b6ae2ff3236c8fe8e5ae
glenn <70140106+ausgore@users.noreply.github.com>
2024-12-10 17:32:43 +0800

    Maslow#reply to expect NEED input

--- src/maslow.ts
+++ src/maslow.ts
@@ -2,24 +2,9 @@ import OpenAI from "openai";
 import dotenv from "dotenv";
 import random from "random";
 import { titleize } from "inflection";
+import { Message, NEEDS } from "./types";
 
 dotenv.config();
-
-enum NEEDS {
-	TRANSCENDENCE = 0,
-	SELF_ACTUALISATION = 1,
-	AESTHETIC = 2,
-	COGNITIVE = 3,
-	ESTEEM = 4,
-	LOVE_AND_BELONGING = 5,
-	SAFETY = 6,
-	BIOLOGICAL_AND_PHYSIOLOGICAL = 7
-}
-
-interface Message {
-	role: "system" | "user";
-	content: string;
-}
 
 class Maslow {
 	private model: string = 'gpt-4o-mini';
@@ -35,12 +20,12 @@ class Maslow {
 			model: this.model,
 			messages: [{
 				role: 'system',
-				content: `This is a conversation simulation between student and lecturer. You are a student, and I am the lecturer. You will need to simulate a conversation as a student, and I will be tasked to guess your needs. We will use the 7 needs of Maslow: Biological and Physiological, Safety, Love and belonging, Esteem, Cognitive, Aesthetic, Self-actualisation, transcendence. When I start the conversation with 'STARTSIMULATION', you will behave as a student who has the need of ${titleize(NEEDS[need])}. When I say 'MASLOWAISTOP' and follow up with the guess of the Maslow need, you will analyse my guess and let me know if it's correct. You, as the student, may preferably address me as 'Cher' which is the short form for Lecturer, or my name if I provide it. You, as a student, must speak in Singlish (Singaporean English), minus the foul language.`
+				content: `This is a conversation simulation between student and lecturer. You are a student, and I am the lecturer. You will need to simulate a conversation as a student, and I will be tasked to guess your needs. We will use the 7 needs of Maslow: Biological and Physiological, Safety, Love and belonging, Esteem, Cognitive, Aesthetic, Self-actualisation, transcendence. You, as the student, may preferably address me as 'Cher' which is the short form for Lecturer, or my name if I provide it. You, as a student, must speak in Singlish (Singaporean English), minus the foul language. When I start the conversation with 'STARTSIMULATION', you will behave as a student who has the need of ${titleize(NEEDS[need])}. When I say 'MASLOWAISTOP' and follow up with the guess of the Maslow need, you will analyse my guess and let me know if it's correct by replying with a sentence that starts with (BOOLEAN) where the answer is whether it's true or false.`
 			}, { role: 'user', content: 'STARTSIMULATION' }]
 		});
 
 		return {
-			need: titleize(NEEDS[need]),
+			need: NEEDS,
 			message: {
 				role: response.choices[0].message.role,
 				content: response.choices[0].message.content
@@ -48,10 +33,13 @@ class Maslow {
 		};
 	}
 
-	reply(content: string, history: Message[]) {
+	reply(content: string, history: Message[], need: NEEDS) {
 		return this.openai.chat.completions.create({
 			model: this.model,
-			messages: [...history, { role: 'user', content }]
+			messages: [{
+				role: 'system',
+				content: `This is a conversation simulation between student and lecturer. You are a student, and I am the lecturer. You will need to simulate a conversation as a student, and I will be tasked to guess your needs. We will use the 7 needs of Maslow: Biological and Physiological, Safety, Love and belonging, Esteem, Cognitive, Aesthetic, Self-actualisation, transcendence. You, as the student, may preferably address me as 'Cher' which is the short form for Lecturer, or my name if I provide it. You, as a student, must speak in Singlish (Singaporean English), minus the foul language. When I start the conversation with 'STARTSIMULATION', you will behave as a student who has the need of ${titleize(NEEDS[need])}. When I say 'MASLOWAISTOP' and follow up with the guess of the Maslow need, you will analyse my guess and let me know if it's correct by replying with a sentence that starts with (BOOLEAN) where the answer is whether it's true or false.`
+			}, ...history, { role: 'user', content }]
 		});
 	}
 }
